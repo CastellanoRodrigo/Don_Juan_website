4700b2fa2b5e9b8a6408a1f1d2bdc849b34cfbdf
Matias Ariel Montiel <axel-gta4@hotmail.com>
2022-11-08 20:59:34 -0300

    PRUEBA
    Realize los cambios en Login

--- login.php
+++ login.php
@@ -1,52 +1,59 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <img src="" src="" alt="">
-    <div class="container">
-        <div class="img">
-        </div>
-           <!-- Envio de datos seguro -->
-        <div class="login-content">
-            <!-- Formulario-metodo de envio-->
-            <form method="post" action="">
-               <!-- imagen de avatar -->
-                <img src="">
-                <h2>Bienvenido</h2>
-                <!--llamar controlador-->
-                <?php
-                include "modelo/conexion.php";
-                include "Controlador/controlador_login.php";
-                ?>
-                <div class="input-div one">
-                    <div class="i">
-                        <i class="fas fa-user"></i>
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" type="text/css" href="Css/estilos.css">
+                <title>Login GAFAS</title>
+            </head>
+            <body>
+                <section class="home">
+                    <div class="BarraNavegacion">
+                        <h1> CAMPO DON JUAN S.R.L.</h1>
+                        <div>
+                            <img src="imagenes/Gafa.jpg">
+                        </div>     
                     </div>
-                    <div class="div">
-                        <h5>Usuario</h5>
-                        <input id="usuario" type="text" class="input" name="usuario">
+                    <!-- Envio de datos seguro -->
+                    <div class="login-content">
+                    <!-- Formulario-metodo de envio-->
+                        <form method="post" action="">
+                            <?php
+                            include "modelo/conexion.php";
+                            include "Controlador/controlador_login.php";
+                            ?>
+                            <div class="cover">
+                                <div class="Content">
+                                    <h1>INICIAR SESION</h1>
+                                    <div class="input-div one">
+                                        <div class="i">
+                                            <i class="fas fa-user"></i>
+                                        </div>
+                                        <div class="div">
+                                            <h5>Usuario</h5>
+                                            <input id="usuario" type="text" class="input" name="usuario">
+                                        </div>
+                                    </div>
+                                    <div class="input-div pass">
+                                        <div class="i">
+                                            <i class="fas fa-lock"></i>
+                                        </div>
+                                        <div class="div">
+                                            <h5>Contraseña</h5>
+                                            <input id="input" type="password" class="input" name="password">
+                                        </div>
+                                    </div>
+                                    <div class="view">
+                                        <div class="fas fa-eve verPassword" onclick="vista()" id="verPassword"></div>
+                                    </div>
+                                    <div>
+                                        <button class="BotonIniciar"> INICIAR SESION </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </div>
-                <div class="input-div pass">
-                    <div class="i">
-                        <i class="fas fa-lock"></i>
-                    </div>
-                    <div class="div">
-                        <h5>Contraseña</h5>
-                        <input id="input" type="password" class="input" name="password">
-                    </div>
-                </div>
-                <div class="view">
-                    <div class="fas fa-eve verPassword" onclick="vista()" id="verPassword"></div>
-                </div>
-                <input name="btningresar" class="btn" type="submit" value="INICIAR SESION">    
-            </form>          
-        </div>
-    
-</body>
-</html>
+                </section>
+            </body>
+        </html>
