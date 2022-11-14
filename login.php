1c8584cf0ced0390db029fb6264c82ed7d3309da
Hernan Altolaguirre <hernan.altola@gmail.com>
2022-11-14 15:48:51 -0300

    agregue required en login

--- login.php
+++ login.php
@@ -32,7 +32,7 @@
                                         </div>
                                         <div class="div">
                                             <h5>Usuario</h5>
-                                            <input id="usuario" type="text" class="input"  name="usuario">
+                                            <input id="usuario" type="text" class="input" required="required"  name="usuario">
                                         </div>
                                     </div>
                                     <div class="input-div pass">
@@ -48,7 +48,7 @@
                                         <div class="fas fa-eve verPassword" onclick="vista()" id="verPassword"></div>
                                     </div>
                                     <div>
-                                        <input name="btningresar" class="BotonIniciar"  type="submit" required="required" value="INICIAR SESION"> 
+                                        <input name="btningresar" class="BotonIniciar"  type="submit" value="INICIAR SESION"> 
                                         <!-- no sirve <button name="btningresar" class="BotonIniciar" class="btn" > INICIAR SESION </button> -->
                                     </div>
                                 </div>
