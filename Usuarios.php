efa24eec3cc0219bb80fd353c71844ee38f363d3
Matias Ariel Montiel <axel-gta4@hotmail.com>
2022-11-14 20:06:20 -0300

    Añadi las nuevas paginas
    Solucione que no aparecían las imágenes de fondo

---
+++ Usuarios.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Css/Usuarios.css">
+    <title>Usuarios</title>
+</head>
+<body>
+<section class="home">
+        <div class="BarraNavegacion">
+            <img src="imagenes/Gafa.jpg">
+            <nav>
+                <a href="*"> Inicio </a>             
+                <a href="*"> Consultas </a>
+                <a href="*"> Usuarios </a>
+            </nav>
+            <div>
+                <button class="BotonSalir"> Salir </button>
+            </div>
+        </div>
+    <section>
+        <div class="Espacio"></div>
+        <div class="Cover2">
+            <div class="ImagenParcela">
+                <h1> USUARIOS </h1>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>IMAGEN NUESTRA</div>
+    </section>
+    <section class="BarraInferior">
+        <h1>Nosotros</h1>
+    </section>
+</body>
+</html>
