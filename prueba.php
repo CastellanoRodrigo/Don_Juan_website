fbead108b4bfb7a4913dd5d6649d91df3db1470c
Rodrigo Castellano <r1989castellano@gmail.com>
2022-11-29 22:33:48 -0300

    parcelas y ha visibles

--- prueba.php
+++ prueba.php
@@ -52,55 +52,67 @@ if (empty($_SESSION["id"])) {
         <div class="container-menu">
             <div class="cont-menu">
                 <nav>
-                <a href="#" class="boton-cerrar" onclick="ocultar()">&times;</a>
-					<ul class="menu">
-						<li><a href="#">Añadir proyecto 1</a></li>
-						<ul class="menu">
-							<li><a href="#">Añadir proyecto hacienda</a></li>
-							<li><a href="#">Añadir proyecto siembra</a></li>
-							<li><a href="#">Añadir detalles hacienda</a></li>
-							<li><a href="#">Añadir detalles hacienda</a></li>
-						</ul>
-						<li><a href="#">Opci�n 2</a></li>
-						<li><a href="#">Opci�n 3</a></li>
-						<li><a href="#">Opci�n 4</a></li>
-						<li><a href="#">Opci�n 5</a></li>
-					</ul>
-<script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script src="./JavaScript/main.js"></script>
+                    <a href="#" class="boton-cerrar" onclick="ocultar()">&times;</a>
+                    <ul class="menu">
+                        <li><a href="#">Añadir proyecto 1</a></li>
+                        <ul class="menu">
+                            <li><a href="#">Añadir proyecto hacienda</a></li>
+                            <li><a href="#">Añadir proyecto siembra</a></li>
+                            <li><a href="#">Añadir detalles hacienda</a></li>
+                            <li><a href="#">Añadir detalles hacienda</a></li>
+                        </ul>
+                        <li><a href="#">Opci�n 2</a></li>
+                        <li><a href="#">Opci�n 3</a></li>
+                        <li><a href="#">Opci�n 4</a></li>
+                        <li><a href="#">Opci�n 5</a></li>
+                    </ul>
+                    <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+                    <script src="./JavaScript/main.js"></script>
                 </nav>
                 <label for="btn-menu">✖️</label>
             </div>
         </div>
         <div style="text-align:center;">
-				<table border="1" style="margin: 0 auto;">
+            <table border="1" style="margin: 0 auto;">
 
-					<h1>INFORMACION DE LAS PARCELAS</h1><br>
-					<div class="containerMap">
-						<!--division  por parcelas -->
-						<map name="mapa">
-							<img src="./imagenes/DonJuanSRL1-MARCADA.png" usemap="#mapa">
-							<area id="area1" class="area" shape="rectangle" coords="494, 690 333, 626" href="parcela1.php">
-							<area id="area2" shape="rectangle" coords="333, 626 497, 478" class="area" href="parcela2.php">
-							<area id="area" shape="rectangle" coords="494, 527 657, 367" class="area" href="parcela3.php" ">
-            				<area id=" area" shape="rectangle" coords="497, 478 334, 365" class="area" href="parcela4.php">
-							<area id="area" shape="rectangle" coords="170, 481 331, 690" class="area" href="parcela5.php">
-							<area id="area" shape="rectangle" coords="334, 365 170, 481" class="area" href="parcela6.php">
-							<area id="area" shape="rectangle" coords="426, 365 667, 251" class="area" href="parcela7.php">
-							<area id="area" shape="rectangle" coords="426, 365 342, 247" class="area" href="parcela8.php">
-							<area id="area" shape="rectangle" coords="342, 247 429, 38" class="area" href="parcela9.php">
-							<area id="area" shape="rectangle" coords="429, 38 542, 250" class="area" href="parcela10.php">
-							<area id="area1" class="area" shape="rectangle" coords="347, 38 257, 163" href="parcela11.php">
-						</map>
-					</div>
-				</table>
-			</div>
+                <h1>INFORMACION DE LAS PARCELAS</h1><br>
+                <div class="containerMap">
+                    <!--division  por parcelas 
+                    • Parcela 1: 43 hectáreas.
+                    • Parcela 2: 90 hectáreas.
+                    • Parcela 3: 100 hectáreas.
+                    • Parcela 4: 70 hectáreas.
+                    • Parcela 5: 100 hectáreas.
+                    • Parcela 6: 70 hectáreas.
+                    • Parcela 7: 100 hectáreas.
+                    • Parcela 8: 35 hectáreas.
+                    • Parcela 9: 65 hectáreas.
+                    • Parcela 10: 95 hectáreas.
+                    • Parcela 11: 30 hectáreas.-->
+
+                    <map name="mapa">
+                        <img src="./imagenes/DonJuanSRL1-MARCADA.png" usemap="#mapa">
+                        <area id="area1" class="area" shape="rectangle" coords="494, 690 333, 626" href="parcela1.php" title="Parcela 1 [43ha]"></area>
+                        <area id="area2" shape="rectangle" coords="333, 626 497, 478" class="area" href="parcela2.php" title="Parcela 2 [90ha]">
+                        <area id="area" shape="rectangle" coords="494, 527 657, 367" class="area" href="parcela3.php" title="Parcela 3 [100ha]">
+                        <area id=" area" shape="rectangle" coords="497, 478 334, 365" class="area" href="parcela4.php" title="Parcela 4 [70ha]">
+                        <area id="area" shape="rectangle" coords="170, 481 331, 690" class="area" href="parcela5.php" title="Parcela 5 [100ha]">
+                        <area id="area" shape="rectangle" coords="334, 365 170, 481" class="area" href="parcela6.php" title="Parcela 6 [70ha]">
+                        <area id="area" shape="rectangle" coords="426, 365 667, 251" class="area" href="parcela7.php" title="Parcela 7 [100ha]">
+                        <area id="area" shape="rectangle" coords="426, 365 342, 247" class="area" href="parcela8.php" title="Parcela 8 [35ha]">
+                        <area id="area" shape="rectangle" coords="342, 247 429, 38" class="area" href="parcela9.php" title="Parcela 9 [65ha]">
+                        <area id="area" shape="rectangle" coords="429, 38 542, 250" class="area" href="parcela10.php" title="Parcela 10 [95ha]">
+                        <area id="area1" class="area" shape="rectangle" coords="347, 38 257, 163" href="parcela11.php" title="Parcela 11 [30ha]">
+                    </map>
+                </div>
+            </table>
+        </div>
         </div>
         <div>
 
         </div>
 
-        
+
 </body>
 
 </html>
