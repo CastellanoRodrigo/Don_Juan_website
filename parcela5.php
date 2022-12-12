48c35d98367af3e4477b81dbeedd6774d4e5e254
Rodrigo Castellano <r1989castellano@gmail.com>
2022-12-12 09:31:22 -0300

    descripción fija parcelas
    Ubicación
    Dimensión
    Casilla para imagen a futuro
    Borrado de las parcelas extra

--- parcela5.php
+++ parcela5.php
@@ -1,55 +1,67 @@
 <title>Don Juan S.R.L detalles</title>
 <?php #Llammo a cabecera, incluye el archivo cabecera.php desde template
-include('./Template/Cabecera.php');?>
+include('./Template/Cabecera.php'); ?>
 
-  <!--llamar controlador-->
+<!--llamar controlador-->
 <?php
 include "modelo/conexion.php";
 // include "Controlador/controlador_login.php";
 ?>
 
-	<br>
-	<center>
+<br>
+
+<center>
 	<br>
 	<h1> Parcela 5 </h1>
+	<div class="descipcion">
+		<div class="descripcion2" position: absolute>
+
+			<p>Ubicación: -36.00664, -63.99208 </p>
+			<p>Dimensión: 100 ha, 1.00 km²</p>
+			<div class="img">
+				<img src="" alt="">
+			</div>
+		</div>
+	</div>
 	<br>
-	</center>
-	<!--ACA IRIA UN TITULO DICIENDO: PROYECTOS DE HACIENDA NO LO PUDE HACER QUEDAR BIEN POR ESO PUSE EL COMENTARIO-->
-	<center>
+</center>
+<!--ACA IRIA UN TITULO DICIENDO: PROYECTOS DE HACIENDA NO LO PUDE HACER QUEDAR BIEN POR ESO PUSE EL COMENTARIO-->
+<center>
 	<br>
 	<table border=2 width=400>
-	<h2> PROYECTO DE HACIENDA </h2>
-	<tr><td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 79px;">NombreProyecto</td>
-	<td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 97px;">FechaInicio</td>
-	<td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 97px;">Fechacierre</td>
-	<td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 86px;">Hectareas</td>
-	<td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 86px;">Cabezas</td>
-	<td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 97px;">Categoria</td>
-	<td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 162px;">InversionInicial</td>
-    <td style= "text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 162px;">Parcela</td>   
-	</tr>
-	</center>
+		<h2> PROYECTO DE HACIENDA </h2>
+		<tr>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 79px;">NombreProyecto</td>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 97px;">FechaInicio</td>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 97px;">Fechacierre</td>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 86px;">Hectareas</td>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 86px;">Cabezas</td>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 97px;">Categoria</td>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 162px;">InversionInicial</td>
+			<td style="text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold; width: 162px;">Parcela</td>
+		</tr>
+</center>
 
-	<?php
-	// conexion con la bd
+<?php
+// conexion con la bd
 
-	$host = "localhost";
-	$usuario = "root";
-	$clave = "";
-	$base = "sistema_dj";
+$host = "localhost";
+$usuario = "root";
+$clave = "";
+$base = "sistema_dj";
 
-	$cn=new mysqli($host, $usuario, $clave, $base);
+$cn = new mysqli($host, $usuario, $clave, $base);
 
-	$registros=$cn->query("SELECT p.NombreProyecto, d.FechaInicio, d.FechaCierre,
+$registros = $cn->query("SELECT p.NombreProyecto, d.FechaInicio, d.FechaCierre,
 						d.CantidadHectareas, d.CantidadCabezas, d.Categoria, d.InversionInicial, p.Id_Parcela 
 						FROM detalleinicialhacienda d
 						INNER JOIN proyectohacienda p ON d.Id_ProyectoHacienda = p.Id_ProyectoHacienda
-						WHERE p.Id_Parcela='5'");	
+						WHERE p.Id_Parcela='5'");
 
-	 while ($myrow=$registros->fetch_row()) //mientras haya registros muestra la informacion
-     {
+while ($myrow = $registros->fetch_row()) //mientras haya registros muestra la informacion
+{
 
-   	   echo "<tr>
+	echo "<tr>
    	   		 <td>$myrow[0]</td>
    	   		 <td>$myrow[1]</td>
    	   		 <td>$myrow[2]</td>
@@ -59,54 +71,54 @@ include "modelo/conexion.php";
    	   		 <td>$myrow[6]</td>
    	   		 <td>$myrow[7]</td>
    	   		 </tr>";
+}
+// se nos habia escapado este if 
+if (mysqli_num_rows($registros) <= 0) {
+	echo "<br>";
+	echo 'No hay proyecto de HACIENDA en esta parcela'; //si no hay mensaje
+	echo "<br>";
+}
 
-     } 	 
-	 // se nos habia escapado este if 
-	if (mysqli_num_rows($registros) <= 0) {
-	  echo "<br>";
-	  echo 'No hay proyecto de HACIENDA en esta parcela'; //si no hay mensaje
-	  echo "<br>";
-	}
+$cn->close();
 
-    $cn->close();
+?>
 
-    ?>
-
-	<!--ACA IRIA UN TITULO DICIENDO: PROYECTOS DE HACIENDA
+<!--ACA IRIA UN TITULO DICIENDO: PROYECTOS DE HACIENDA
 		NO LO PUDE HACER QUEDAR BIEN POR ESO PUSE EL COMENTARIO-->
-	<center>
+<center>
 	<br>
 	<table border=2 width=400>
-	<br>
-	<br>
-	<h2> PROYECTO DE SIEMBRA </h2>	
-	<tr><td style= text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>NombreProyecto</td>
-	<td style= text-align:center;font-size:16pt;height:30px;background-color:lightgreen;font-weight:bold>FechaInicio</td>
-	<td style= text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>FechaCierre</td>
-    <td style= text-align:center;font-size:16pt;height:30px;background-color:lightgreen;font-weight:bold>Hectareas</td>
-	<td style= text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>Cultivo </td>
-	<td style= text-align:center;font-size:16pt;height:30px;background-color:lightgreen;font-weight:bold>RindeEspeculado</td>
-	<td style= text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>InversionInicial</td>
-	<td style= text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>Parcela</td>
-	</tr>
-	</center>
+		<br>
+		<br>
+		<h2> PROYECTO DE SIEMBRA </h2>
+		<tr>
+			<td style=text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>NombreProyecto</td>
+			<td style=text-align:center;font-size:16pt;height:30px;background-color:lightgreen;font-weight:bold>FechaInicio</td>
+			<td style=text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>FechaCierre</td>
+			<td style=text-align:center;font-size:16pt;height:30px;background-color:lightgreen;font-weight:bold>Hectareas</td>
+			<td style=text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>Cultivo </td>
+			<td style=text-align:center;font-size:16pt;height:30px;background-color:lightgreen;font-weight:bold>RindeEspeculado</td>
+			<td style=text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>InversionInicial</td>
+			<td style=text-align:center;font-size:16pt;background-color:lightgreen;font-weight:bold;>Parcela</td>
+		</tr>
+</center>
 
-	<?php
+<?php
 
-    $cn= new mysqli("localhost", "root", "", "sistema_dj"); //conexion con base de datos
+$cn = new mysqli("localhost", "root", "", "sistema_dj"); //conexion con base de datos
 
 
-    $registros= $cn->query("SELECT p.NombreProyecto, d.FechaInicio, d.FechaCierre, d.CantidadHectareas, s.NombreCultivo,
+$registros = $cn->query("SELECT p.NombreProyecto, d.FechaInicio, d.FechaCierre, d.CantidadHectareas, s.NombreCultivo,
  								   d.RindeEspeculado, d.InversionInicial, p.Id_Parcela
   							FROM detalleinicialsiembra d
  						    INNER JOIN proyectosiembra p ON d.Id_ProyectoSiembra = p.Id_ProyectoSiembra
   							INNER JOIN siembra s ON d.Id_Cultivo = s.Id_Cultivo
- 						    WHERE p.Id_Parcela='5'");    
-    
-     while ($myrow=$registros->fetch_row()) //mientras haya registros muestra
-     {
-      	    
-   	   echo "<tr>
+ 						    WHERE p.Id_Parcela='5'");
+
+while ($myrow = $registros->fetch_row()) //mientras haya registros muestra
+{
+
+	echo "<tr>
    	         <td> $myrow[0] </td>
    	         <td> $myrow[1] </td>
    	         <td> $myrow[2] </td>
@@ -116,22 +128,21 @@ include "modelo/conexion.php";
    	         <td> $myrow[6] </td>
    	         <td> $myrow[7] </td> 	   
    	         </tr>";
+}
+if (mysqli_num_rows($registros) <= 0) {
+	echo "<br>";
+	echo "<br>";
+	echo 'No hay proyecto de SIEMBRA en esta parcela'; //si no hay mensaje
+}
 
-     } 
-     if (mysqli_num_rows($registros) <= 0) {
-	   echo "<br>";
-	   echo "<br>";
-       echo 'No hay proyecto de SIEMBRA en esta parcela'; //si no hay mensaje
-     }
-     
-	// Cierra conexion
-    $cn->close();
+// Cierra conexion
+$cn->close();
 
-	?>
-	<br>
-	<br>
-	<br>
-	<br>
+?>
+<br>
+<br>
+<br>
+<br>
 </body>
 
 <?php #Llammo a pie 
