f950831a752d0715e8e6e9482b71fcbaa8b7f934
HernanAltola22 <hernan.altola@gmail.com>
2023-03-21 18:09:19 -0300

    borre todo

--- Cambios_Maty/Conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-#conexion a base de datos, creando la variable "conexion"
-#en localhost, al subir a prod,se coloca la dir ip, seguido del usuario,contraseña y la base de datos
-$conexion=new mysqli("localhost","root","","sistema_dj");
-$conexion->set_charset("utf8");
-
-?>
--- Modelo/Conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$conexion=mysqli_connect('localhost:3307','root','','sistema_dj'); 
-/* NUEVA BASE DE DATOS*/
-$conexion=mysqli_connect('localhost:3307','root','','donjuandb'); 
-?>
